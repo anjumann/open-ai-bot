acfde315a54d0c52f13c5d5aeae69cd6b2a9b241
Anjuman Raj <ayusmann23@gmail.com>
2023-05-25 21:30:43 +0530

    🚀 chore(package.json): add openai package as dependency 🆕 feat(openai.js): add openai completion function to generate 5 random sentences 🔧 chore(index.js): remove default Next.js homepage content 🎨 style(globals.css): remove custom background and foreground colors, use Tailwind CSS defaults

---
+++ src/pages/api/openai.js
@@ -0,0 +1,44 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+
+const openAiCompletion = async (message) => {
+
+    try {
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                // { role: "system", content: "answer as concisely as possible. act like chatgpt " },
+                { role: "user", content: "generate a 5 random sentence " }
+            ],
+            
+        });
+        console.log('====================================');
+        console.log(response.data.choices[0].message.content);
+        console.log('====================================');
+        return response.data.choices[0].message.content;
+    }
+    catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+
+export default async function handler(req, res) {
+
+    // const { message } = req.body;
+
+    if (req.method !== 'POST') {
+        res.status(405).json({ message: 'Only POST requests allowed' })
+    }
+
+    const message =  "generate a 5 random sentence "
+    const data = await openAiCompletion(message);
+    res.status(200).json({ data: data })
+}
